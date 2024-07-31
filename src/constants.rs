0dc9e2906abe5491d8e2ee0a95835923cba9da9b
KevKibe <kchegz234@gmail.com>
2024-07-31 21:02:08 +0300

    add constant variables file

---
+++ src/constants.rs
@@ -0,0 +1,15 @@
+//! Constants used throughout the codebase
+//!
+//! 
+pub const TRACK_DATA_DIR: &str = ".trackdata";
+
+pub const DEFAULT_BRANCH_NAME: &str = "main";
+pub const INITIAL_COMMIT_MSG: &str = "Initialized Repo";
+pub const REPO_CONFIG_FILENAME: &str = "config.toml";
+pub const DIRS_DIR: &str = "dirs";
+pub const STAGED_DIR: &str = "staged";
+pub const SCHEMAS_DIR: &str = "schemas";
+pub const COMMITS_DIR: &str = "commits";
+pub const USER_CONFIG_FILENAME: &str = "user_config.toml";
+pub const REFS_DIR: &str = "refs";
+pub const HEAD_FILE: &str = "HEAD";
